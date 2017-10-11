904b5fceb74f297f90f4463e796500933ce16128
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-10-11 11:16:00 -0500

    tip calculator application created

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
+//Greetings
+
 app.get('/greeting/:id', (req, res) => {
 	res.send('Welcome ' + req.params.id);
 })
+
+//Tip Calculator
+
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  console.log(req.params.total);
+  console.log(req.params.tipPercentage);
+  let tot = parseInt(req.params.total);
+  let per = parseInt(req.params.tipPercentage);
+  let answ = tot * per / 100;
+  let text = answ.toString();
+  console.log(text);
+  res.send(text);
+});
 
 app.listen(3000, () => {
 	console.log('app is listening on port 3000');
