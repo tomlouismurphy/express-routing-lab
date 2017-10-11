96276cf9b5c37afd60186f214b51a24167f6ff97
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-10-11 11:31:47 -0500

    magic 8 ball express application created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 //Greetings
 
@@ -8,7 +9,6 @@ app.get('/greeting/:id', (req, res) => {
 })
 
 //Tip Calculator
-
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
   console.log(req.params.total);
@@ -20,6 +20,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   console.log(text);
   res.send(text);
 });
+
+//Magic 8-Ball
+
+app.get('/magic/:question', (req, res) => {
+	const answer = Math.floor(Math.random() * eightBall.length)
+	res.send(req.params.question + '?' + '\n' + eightBall[answer]);
+})
 
 app.listen(3000, () => {
 	console.log('app is listening on port 3000');
